58d50b67c53015ee0d25589658e1ed08f6575ed9
YuriFox <yuri17fox@gmail.com>
2019-02-11 18:18:31 +0200

    v 0.1

---
+++ Source/AirCollection.h
@@ -0,0 +1,19 @@
+//
+//  AirCollection.h
+//  AirCollection
+//
+//  Created by Yuri Fox on 2/11/19.
+//  Copyright © 2019 Developer Lysytsia. All rights reserved.
+//
+
+//#import <UIKit/UIKit.h>
+
+//! Project version number for AirCollection.
+//FOUNDATION_EXPORT double AirCollectionVersionNumber;
+
+//! Project version string for AirCollection.
+//FOUNDATION_EXPORT const unsigned char AirCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirCollection/PublicHeader.h>
+
+
