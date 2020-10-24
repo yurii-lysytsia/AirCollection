05937c043dcfcd3e75ad0e14d7bc4eb94d50874e
Yuri <yuri17fox@gmail.com>
2020-10-24 09:33:43 +0300

    Added collection view index path for supplementary view

--- Source/CollectionView/CollectionView+Extension.swift
+++ Source/CollectionView/CollectionView+Extension.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Developer Lysytsia. All rights reserved.
 //
 
+import struct Foundation.IndexPath
 import class UIKit.UICollectionView
 import class UIKit.UICollectionViewCell
 import class UIKit.UICollectionReusableView
@@ -30,6 +31,17 @@ public extension UICollectionView {
     /// Registers a nib file for use in creating supplementary views for the collection view
     func register<T: UICollectionReusableView>(_ viewClass: T.Type, for kind: CollectionViewElementKindSection) where T: NibLoadableView & IdentificableView {
         self.register(viewClass.viewNib, forSupplementaryViewOfKind: kind.rawValue, withReuseIdentifier: viewClass.viewIdentifier)
+    }
+    
+    /// Gets the index path of supplementary view of the specified type.
+    /// - Parameters:
+    ///   - supplementaryView: The supplementary view object whose index path you want.
+    ///   - kind: The kind of supplementary view to locate. This value is defined by the layout object. This parameter must not be nil
+    func indexPath(forSupplementaryView supplementaryView: UICollectionReusableView, of elementKind: CollectionViewElementKindSection) -> IndexPath? {
+        let visibleIndexPaths = self.indexPathsForVisibleSupplementaryElements(ofKind: elementKind.rawValue)
+        return visibleIndexPaths.first(where: {
+            self.supplementaryView(forElementKind: elementKind.rawValue, at: $0) == supplementaryView
+        })
     }
     
 }
